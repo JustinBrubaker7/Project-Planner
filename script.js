333c24fcb9c99544f37f09c8791d7bd264b50317
Daniel Moore <wron2@hotmail.com>
2021-03-01 20:47:44 -0800

    adding Task 2 and some task 3

---
+++ script.js
@@ -0,0 +1,53 @@
+var projName = $('#project-name');
+var projType = $('#project-type');
+var hourWage = $('#hourWage');
+var dueDate = $('#due-date');
+var thisForm = $('<form>');
+
+daysUntilDue;
+
+function addUser(){
+
+    //Insert location where submitted info is to be added
+    $('#final-form').append( "<tr>" +
+    "<td>" + projName.val() + "</td>" +
+    "<td>" + projType.val() + "</td>" +
+    "<td>" + hourWage.val() + "</td>" +
+    "<td>" + dueDate.val() + "</td>" +
+    "<td>" + daysUntilDue + "</td>" +
+    "</tr>" );
+    dialog.dialog( "close" );
+    
+}
+
+// Instantiates the popup window 
+dialog = $( "#dialog-form" ).dialog({
+    autoOpen: false,
+    height: 400,
+    width: 350,
+    modal: true,
+    buttons: {
+      "Create an account": addUser,
+      Cancel: function() {
+        dialog.dialog( "close" );
+      }
+    },
+    close: function() {
+      form[ 0 ].reset();
+      allFields.removeClass( "ui-state-error" );
+    }
+  });
+
+thisForm.addEventListener("submit", function(event){
+
+    event.preventDefault();
+    addUser();
+
+})
+
+// Button to open up the submission form
+$( "#create-user" ).button().on( "click", function() {
+    dialog.dialog( "open" );
+});
+
+
